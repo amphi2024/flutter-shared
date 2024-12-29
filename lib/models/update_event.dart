ff1e3daf0834f7655d6928976b440afc24a9c453
Amphi <amphiprioninae2024@gmail.com>
2024-12-29 12:37:25 +0900

    commit

--- lib/models/update_event.dart
+++ lib/models/update_event.dart
@@ -8,12 +8,14 @@ class UpdateEvent {
     static const String uploadVideo = "upload_video";
     static const String uploadTheme = "upload_theme";
     static const String uploadColors = "upload_colors";
+    static const String uploadFile = "upload_file";
 
     static const String deleteNote = "delete_note";
     static const String deleteImage = "delete_image";
     static const String deleteVideo = "delete_video";
     static const String deleteTheme = "delete_theme";
     static const String deleteColors = "delete_colors";
+    static const String deleteFile = "delete_file";
     static const String renameUser = "rename_user";
 
     // websocket only
