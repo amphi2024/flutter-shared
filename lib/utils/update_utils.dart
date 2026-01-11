82947a8f7f8495ecf717ba19356bdcc4e17eb8e4
siar1234 <siar019283@gmail.com>
2026-01-11 17:28:08 +0900

    feat: add onSuccess callback

--- lib/utils/update_utils.dart
+++ lib/utils/update_utils.dart
@@ -1,10 +1,13 @@
 import 'package:amphi/utils/try_json_decode.dart';
 import 'package:http/http.dart';
 
-Future<void> checkForUpdate({required String url, required String currentVersion, required void Function(String version) onUpdateFound, void Function()? onFailed}) async {
+Future<void> checkForUpdate({required String url, required String currentVersion, void Function()? onSuccess, required void Function(String version) onUpdateFound, void Function()? onFailed}) async {
   try {
     final response = await get(Uri.parse(url)).timeout(const Duration(seconds: 5));
     final body = tryJsonDecode(response.body, defaultValue: {});
+    if(response.statusCode == 200) {
+      onSuccess?.call();
+    }
     final tagName = body['tag_name'];
     if(tagName is String) {
       final latestVersion = tagName.replaceAll("v", "");
